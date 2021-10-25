47a7fd145c3b3848eba975afbc1abc0d2e1b15be
BigZet <bigzet200120@mail.ru>
2021-10-25 18:54:21 +0300

    init

---
+++ arduino_secrets.h
@@ -0,0 +1,9 @@
+// Fill in your WiFi networks SSID and password
+#define SECRET_SSID         "MGTS_GPON_4E77"
+#define SECRET_PASS         "ADXXPeXD"
+
+// Fill in your Google Cloud Platform - IoT Core info
+#define SECRET_PROJECT_ID   "arduino-nano-iot-33-327219"
+#define SECRET_CLOUD_REGION "europe-west1"
+#define SECRET_REGISTRY_ID  "heartrate_nano_iot"
+#define SECRET_DEVICE_ID    "Nano33IoT"
